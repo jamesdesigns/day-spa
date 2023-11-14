1938aa291e89befb6c58ec67378d24e285d74fe7
James Hooper <jameshooperdesigns@gmail.com>
2023-11-14 10:57:09 -0700

    Add new responsive navigation

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 // app.js
 // Add your JavaScript code here if needed
+document.addEventListener('DOMContentLoaded', function () {
+  const burgerMenu = document.querySelector('.burger-menu');
+  const navLinks = document.querySelector('.nav-links');
+
+  burgerMenu.addEventListener('click', function () {
+    navLinks.classList.toggle('show');
+  });
+});
