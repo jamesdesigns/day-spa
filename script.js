a52022b20cecc7d15841e6b46a75ebc9ba206b4b
James Hooper <jameshooperdesigns@gmail.com>
2023-11-14 11:51:55 -0700

    Add background image to hero

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 // app.js
 // Add your JavaScript code here if needed
-document.addEventListener('DOMContentLoaded', function () {
-  const burgerMenu = document.querySelector('.burger-menu');
-  const navLinks = document.querySelector('.nav-links');
 
-  burgerMenu.addEventListener('click', function () {
-    navLinks.classList.toggle('show');
-  });
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const burgerMenu = document.querySelector('.burger-menu');
+//   const navLinks = document.querySelector('.nav-links');
+
+//   burgerMenu.addEventListener('click', function () {
+//     navLinks.classList.toggle('show');
+//   });
+// });
+
+$('.navTrigger').click(function () {
+  $(this).toggleClass('active');
+  console.log("Clicked menu");
+  $("#mainListDiv").toggleClass("show_list");
+  $("#mainListDiv").fadeIn();
+
 });
